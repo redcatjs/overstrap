f479e74af96e5ac056e7e925c27fdb51ab95ae1b
jo <jo@surikat.pro>
2017-02-28 13:01:42 +0100

    wip

--- overstrap.js
+++ overstrap.js
@@ -27,60 +27,78 @@
 		}
 	}
 	
-
-	jstack.loader('input[autofocus]',function(){
-		$(this).siblings('label, i').addClass('active');
-	});
-
-	jstack.loader('.animated-bar',function(){
-		var $el = $(this);
-		if(!$el.find('.bar').length){
-			$el.append('<span class="bar" />');
-		}
-	});
-	
-	jstack.loader(input_selector,function(){
-		var $el = $(this);
-		var elements = $el.siblings('label, i');
-		if($el.val().length > 0 || $el.attr('placeholder') !== undefined || this.validity.badInput === true){
-			elements.addClass('active');
-		}
-		else{
-			elements.removeClass('active');
-		}
-		$el.on('change', function(){
-			if ($el.val().length !== 0 || $el.attr('placeholder') !== undefined) {
-				elements.addClass('active');
+	$.overstrap = function(options){
+		options = $.extend(true,{
+			animatedBar: true,
+		},options);
+		
+		
+		jstack.loader('.input-field',function(){
+			let $el = $(this);
+			let autofocus = $el.find('input[autofocus]');
+			if(autofocus.length){
+				autofocus.siblings('label, i').addClass('active');
 			}
-			validate_field($el);
-		});
-		$el.on('focus', function () {
-			elements.addClass('active');
-		});
-		$el.on('blur', function () {
-			if ($el.val().length === 0 && this.validity.badInput !== true && $el.attr('placeholder') === undefined) {
-				elements.removeClass('active');
+			
+			let input = $el.find(input_selector);
+			if(input.length){
+				if(options.animatedBar){
+					$el.addClass('animated-bar');
+					if(!$el.find('.bar').length){
+						$el.append('<span class="bar" />');
+					}
+				}
+				
+				input.each(function(){
+					var $el = $(this);
+					var elements = $el.siblings('label, i');
+					if($el.val().length > 0 || $el.attr('placeholder') !== undefined || this.validity.badInput === true){
+						elements.addClass('active');
+					}
+					else{
+						elements.removeClass('active');
+					}
+					$el.on('change', function(){
+						if ($el.val().length !== 0 || $el.attr('placeholder') !== undefined) {
+							elements.addClass('active');
+						}
+						validate_field($el);
+					});
+					$el.on('focus', function () {
+						elements.addClass('active');
+					});
+					$el.on('blur', function () {
+						if ($el.val().length === 0 && this.validity.badInput !== true && $el.attr('placeholder') === undefined) {
+							elements.removeClass('active');
+						}
+						validate_field($el);
+					});
+				});
+				
 			}
-			validate_field($el);
+			
+			
+			
 		});
-	});
-	
-	
-	$.on('reset', 'form', function (e) {
-		var formReset = $(e.target);
-		formReset.find(input_selector).removeClass('valid').removeClass('invalid');
-		formReset.find(input_selector).each(function () {
-			if ($(this).attr('value') === '') {
-				$(this).siblings('label, i').removeClass('active');
-			}
-		});
+		
+		
+		
+		$.on('reset', 'form', function (e) {
+			var formReset = $(e.target);
+			formReset.find(input_selector).removeClass('valid').removeClass('invalid');
+			formReset.find(input_selector).each(function () {
+				if ($(this).attr('value') === '') {
+					$(this).siblings('label, i').removeClass('active');
+				}
+			});
 
-		// Reset select
-		formReset.find('select.initialized').each(function () {
-			var reset_text = formReset.find('option[selected]').text();
-			formReset.siblings('input.select-dropdown').val(reset_text);
+			// Reset select
+			formReset.find('select.initialized').each(function () {
+				var reset_text = formReset.find('option[selected]').text();
+				formReset.siblings('input.select-dropdown').val(reset_text);
+			});
 		});
-	});
-
+		
+	};
 
 }());
