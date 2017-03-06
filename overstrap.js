e76ae961b8c44ae2fadab5683e45e2ec64935c8e
jo <jo@surikat.pro>
2017-03-06 11:48:27 +0100

    wip

--- overstrap.js
+++ overstrap.js
@@ -8,10 +8,10 @@
 			return input.is(':valid');
 		}
 	};
-	
+
 	$.overstrap = function(options){
 		options = $.extend(true,defaultOptions,options);
-		
+
 		function input_validate(el){
 			el = $(el);
 			if((options.autoValidate && !el.hasClass('no-validate')) || el.hasClass('validate')){
@@ -23,7 +23,7 @@
 				}
 			}
 		}
-		
+
 		function input_filled(el){
 			if(el.tagName.toLowerCase()=='input'&&( el.type=='checkbox'||el.type=='radio' )){
 				return $(el).prop('checked');
@@ -32,36 +32,36 @@
 				return Boolean( $(el).val().length );
 			}
 		}
-		
+
 		jstack.loader('.input-field',function(){
 			let $el = $(this);
 			let autofocus = $el.find('input[autofocus]');
 			if(autofocus.length){
 				autofocus.siblings('label, i').addClass('active');
 			}
-			
+
 			let inputs = $el.find('input, textarea');
-			
+
 			if(inputs.length){
-				
+
 				if(options.animatedBar && inputs.filter(input_selector).length){
 					$el.addClass('animated-bar');
 					if(!$el.find('.bar').length){
 						$el.append('<span class="bar" />');
 					}
 				}
-				
+
 				inputs.each(function(){
 					let input = $(this);
 					let val = input_filled(this);
-					
+
 					if(val){
 						input_validate(this);
 					}
 					if(val || input.is(':focus')){
 						$el.addClass('active');
 					}
-					
+
 					let id = input.requiredId();
 
 					input.nextUntil('input','label').each(function(){
@@ -69,7 +69,7 @@
 							this.setAttribute('for',id);
 						}
 					});
-					 
+
 					input
 						.on('change blur reset', function(e){
 							if(input_filled(this)){
@@ -88,18 +88,18 @@
 						})
 					;
 				});
-				
+
 			}
-			
-			
-			
+
+
+
 		});
-		
-		
+
+
 		$.on('reset', 'form', function(){
 			$(this).find(input_selector).trigger('reset');
 		});
-		
+
 		//bootstrap 4 bugfix !
 		$.on('mousedown','.dropdown-menu *',function(e){
 			return false;
