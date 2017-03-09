b7f926da3114a6187f15b5d8bb2fcc355d892a4c
jo <jo@surikat.pro>
2017-03-09 09:25:00 +0100

    refactor

--- overstrap.js
+++ overstrap.js
@@ -8,142 +8,152 @@
 			return $(input).is(':valid');
 		}
 	};
+	
+	var overstrap = {
+		
+	};
+	
+	overstrap.inputFilled = function(el){
+		if(el.tagName.toLowerCase()=='input'&&( el.type=='checkbox'||el.type=='radio' )){
+			return $(el).prop('checked');
+		}
+		else{
+			return Boolean( $(el).val().length );
+		}
+	};
+	
+	overstrap.validateField = function(field, isInit){
+		
+		let self = this;
+		
+		field = $(field);
 
-	$.overstrap = function(options){
-		options = $.extend(true,defaultOptions,options);
-
-		function field_validate(field, isInit){
-
-			field = $(field);
-
-			if(!field.hasClass('validate')){
-				return;
-			}
-
-			let isValid = true;
-			let isFilled = false;
-
-			field.find(':input[name]').each(function(){
-				let input = $(this);
-				if(isInit&&!isFilled&&input_filled(this)){
-					isFilled = true;
-				}
-				if(!options.validate(this)){
-					isValid = false;
-					input.removeClass('form-control-success').addClass('form-control-danger');
-				}
-				else{
-					input.removeClass('form-control-danger').addClass('form-control-success');
-				}
-			});
-
-			if(!isInit||isFilled){
-				if(isValid){
-					field.removeClass('has-danger invalid').addClass('has-success valid');
-				}
-				else{
-					field.removeClass('has-success valid').addClass('has-danger invalid');
-				}
-			}
-
+		if(!field.hasClass('validate')){
+			return;
 		}
 
-		function input_filled(el){
-			if(el.tagName.toLowerCase()=='input'&&( el.type=='checkbox'||el.type=='radio' )){
-				return $(el).prop('checked');
+		let isValid = true;
+		let isFilled = false;
+
+		field.find(':input[name]').each(function(){
+			let input = $(this);
+			if(isInit&&!isFilled&&self.inputFilled(this)){
+				isFilled = true;
+			}
+			if(!self.options.validate(this)){
+				isValid = false;
+				input.removeClass('form-control-success').addClass('form-control-danger');
 			}
 			else{
-				return Boolean( $(el).val().length );
+				input.removeClass('form-control-danger').addClass('form-control-success');
+			}
+		});
+
+		if(!isInit||isFilled){
+			if(isValid){
+				field.removeClass('has-danger invalid').addClass('has-success valid');
+			}
+			else{
+				field.removeClass('has-success valid').addClass('has-danger invalid');
 			}
 		}
 
-		jstack.loader('.input-field',function(){
-			
-			let el = this;
-			let $el = $(this);
-			let autofocus = $el.find('input[autofocus]');
-			if(autofocus.length){
-				autofocus.siblings('label, i').addClass('active');
-			}
+	};
+	
+	overstrap.loadInputField = function(el){
+		let self = this;
+		
+		let $el = $(el);
+		let autofocus = $el.find('input[autofocus]');
+		if(autofocus.length){
+			autofocus.siblings('label, i').addClass('active');
+		}
 
-			let inputs = $el.find('input, textarea');
-			
-			if(options.autoValidate&&!$el.hasClass('no-validate')){
-				$el.addClass('validate');
-			}
-			
-			if(inputs.length){
+		let inputs = $el.find('input, textarea');
+		
+		if(self.options.autoValidate&&!$el.hasClass('no-validate')){
+			$el.addClass('validate');
+		}
+		
+		if(inputs.length){
 
-				if(options.animatedBar){
-					let underlinableInputs = inputs.filter(input_selector);
-					if(underlinableInputs.length){
-						$el.addClass('animated-bar');
-						if(!$el.find('.bar').length){
-							underlinableInputs.each(function(){
-								$(this).next('label').after('<span class="bar" />');
-							});
-						}
+			if(self.options.animatedBar){
+				let underlinableInputs = inputs.filter(input_selector);
+				if(underlinableInputs.length){
+					$el.addClass('animated-bar');
+					if(!$el.find('.bar').length){
+						underlinableInputs.each(function(){
+							$(this).next('label').after('<span class="bar" />');
+						});
 					}
 				}
+			}
+			
+
+			self.validateField(el, true);
+
+			inputs.each(function(){
+				let input = $(this);
+				let val = self.inputFilled(this);
+				let isFirstFilling = !val;
 				
-
-				field_validate(el, true);
-
-				inputs.each(function(){
-					let input = $(this);
-					let val = input_filled(this);
-					let isFirstFilling = !val;
-					
-					if(this.type=='checkbox'){
-						if(!input.next().is('label')){
-							input.after('<label></label>');
-						}
+				if(this.type=='checkbox'){
+					if(!input.next().is('label')){
+						input.after('<label></label>');
 					}
+				}
 
-					if(val || input.is(':focus')){
-						$el.addClass('active');
+				if(val || input.is(':focus')){
+					$el.addClass('active');
+				}
+
+				let id = input.requiredId();
+
+				input.nextUntil('input','label').each(function(){
+					if(!this.hasAttribute('for')){
+						this.setAttribute('for',id);
 					}
-
-					let id = input.requiredId();
-
-					input.nextUntil('input','label').each(function(){
-						if(!this.hasAttribute('for')){
-							this.setAttribute('for',id);
-						}
-					});
-
-					input
-						.on('change blur reset', function(e){
-							if(input_filled(this)){
-								$el.addClass('active');
-							}
-							else{
-								$el.removeClass('active');
-							}
-							isFirstFilling = false;
-							field_validate(el);
-						})
-						.on('input', function(e){
-							if(!isFirstFilling&&$(this).is(input_selector)){
-								field_validate(el);
-							}
-						})
-						.on('focus', function(){
-							$el.addClass('active');
-						})
-					;
 				});
 
-			}
+				input
+					.on('change blur reset', function(e){
+						if(self.inputFilled(this)){
+							$el.addClass('active');
+						}
+						else{
+							$el.removeClass('active');
+						}
+						isFirstFilling = false;
+						self.validateField(el);
+					})
+					.on('input', function(e){
+						if(!isFirstFilling&&$(this).is(input_selector)){
+							self.validateField(el);
+						}
+					})
+					.on('focus', function(){
+						$el.addClass('active');
+					})
+				;
+			});
+
+		}
 
 
 
+	};
+	
+	$.overstrap = function(options){
+		
+		overstrap.options = $.extend(true,defaultOptions,options);
+
+		jstack.loader('.input-field',function(){
+			overstrap.loadInputField(this);
 		});
-
 
 		$.on('reset', 'form', function(){
 			$(this).find('.input-field').each(function(){
-				field_validate(this, true);
+				overstrap.validateField(this, true);
 			});
 		});
 
@@ -152,5 +162,5 @@
 			return false;
 		});
 	};
-
+	
 }());
